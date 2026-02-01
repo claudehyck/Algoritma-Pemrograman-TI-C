3c58cc12ca5c1165fc923b2ad828995f6a074d43
yohanes-Tambunan <yohanes.doly4585@student.unri.ac.id>
2026-02-01 19:40:12 +0700

    numbers.py

--- pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
+++ pertemuan-1/latihan-praktikum/tempCodeRunnerFile.py
@@ -1,57 +1,14 @@
-#Tipe data pada python
-#Variabel dapat menyimpan data dengan tipe yang berbeda, dan tipe yang berbeda dapat melakukan hal yang berbeda pula.
-#Python memiliki tipe data bawaan berikut secara default, dalam kategori-kategori ini:
-#Tipe Teks: str
-#Tipe Numerik: int, float, complex
-#Tipe Urutan: list, tuple, range
-#Tipe Pemetaan: dict
-#Tipe Himpunan: set, frozenset
-#Tipe Boolean: bool
-#Tipe Biner: bytes, bytearray, memoryview
-#Tipe None: NoneType
+#ada tiga tipe data angka di python
+#int = bilangan bulat
+#float = bilangan real
+#complex = merepresentasikan bilangan kompleks yang terdiri dari bagian riil dan imajiner
 
-#str
-x = str("Hello World")
-print(str(x)) 
 #int
-x = int(20)
-print(int(x)) 
+x= 1
+print (int(x))
 #float
-x = float(20.5)
-print(float(x)) 
+x=  1.10
+print (float(x))
 #complex
-x = complex(1j)
-print(complex(x)) 
-#list
-x = list(("apple", "banana", "cherry"))
-print(list(x)) 
-#tuple
-x = tuple(("apple", "banana", "cherry"))
-print(tuple(x))
-#range
-x = range (3,10)
-print (list(x))
-#dict
-x = dict(name="John", age=36)
-print(dict(x)) 
-#set
-x = set(("apple", "banana", "cherry"))
-print(set(x)) 
-#frozenset
-x = frozenset(("apple", "banana", "cherry"))
-print(frozenset(x)) 
-#bool
-x = bool(5)
-print(bool(x)) 
-#bytes
-x = bytes(5)
-print(bytes(x)) 
-#bytearray
-x = bytearray(5)
-print(bytearray(x)) 
-#memoryview
-x = memoryview(bytes(5))
-print(memoryview(x))
-#nonetype
-x = None
-print(type(x)) 
+x= 3+5j
+print(complex(x))
